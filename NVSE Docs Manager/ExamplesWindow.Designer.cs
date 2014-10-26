8d0dc33d34bd81198da67e9e306267c805936f75
Gribbleshnibit8 <gribbleshnibit8@gmail.com>
2014-10-25 22:33:47 -0500

    Initial commit
    Added Examples Window for handling the function examples.

---
+++ NVSE Docs Manager/ExamplesWindow.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace NVSE_Docs_Manager
+{
+	partial class ExamplesWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Items.AddRange(new object[] {
+            "Example 1",
+            "Example 2"});
+			this.listBox1.Location = new System.Drawing.Point(12, 12);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.ScrollAlwaysVisible = true;
+			this.listBox1.Size = new System.Drawing.Size(135, 108);
+			this.listBox1.TabIndex = 0;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.AcceptsTab = true;
+			this.richTextBox1.EnableAutoDragDrop = true;
+			this.richTextBox1.Location = new System.Drawing.Point(153, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(469, 288);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 126);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(135, 47);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Add Example";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 272);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(135, 28);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Done";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// ExamplesWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(634, 312);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "ExamplesWindow";
+			this.Text = "ExamplesWindow";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
